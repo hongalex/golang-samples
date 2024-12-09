632148223ad4399abac3ee86b0fef70830576f3c
Alex Hong <9397363+hongalex@users.noreply.github.com>
2024-12-09 14:34:07 -0800

    feat(pubsub): add new create topic sample

---
+++ pubsub/topics/create_new.go
@@ -0,0 +1,47 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package topics
+
+// [START pubsub_create_topic]
+import (
+	"context"
+	"fmt"
+	"io"
+
+	pubsub "cloud.google.com/go/pubsub/apiv1"
+	"cloud.google.com/go/pubsub/apiv1/pubsubpb"
+)
+
+func createTopicNew(w io.Writer, projectID, topicID string) error {
+	// projectID := "my-project-id"
+	// topicID := "my-topic"
+	ctx := context.Background()
+	client, err := pubsub.NewPublisherClient(ctx)
+	if err != nil {
+		return fmt.Errorf("pubsub.NewPublisherClient: %w", err)
+	}
+	defer client.Close()
+
+	req := &pubsubpb.Topic{
+		Name: fmt.Sprintf("projects/%s/topics%s", projectID, topicID),
+	}
+	_, err = client.CreateTopic(ctx, req)
+	if err != nil {
+		return fmt.Errorf("CreateTopic: %w", err)
+	}
+	return nil
+}
+
+// [END pubsub_create_topic]
